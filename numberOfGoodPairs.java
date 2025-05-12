c18a0aabd4c96c378c9c469a9da3e759f908b293
AnilYadav-7 <padianilyadav123@gmail.com>
2025-05-12 09:46:12 +0530

    Brute Force programs

---
+++ numberOfGoodPairs.java
@@ -0,0 +1,21 @@
+package com.DSA.Brute;
+
+public class numberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    private static int numIdenticalPairs(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
